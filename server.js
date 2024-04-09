8712c691fe71e834420703dec2181bab4046f12d
Naveenmaddy7081 <85430074+Naveenmaddy7081@users.noreply.github.com>
2024-04-10 00:38:01 +0530

    new server update

--- server.js
+++ server.js
@@ -1,32 +1,8 @@
-// var fs = require('fs');
-// var os = require('os');
 
-// var user =os.userInfo();
-// console.log(user);
-// console.log(user.username);
 
-// fs.appendFile('getting.txt','Hii'+user.username+'!\n',()=>{
-//     console.log("file is created");
-// });
 
-// // console.log(os);
-// console.log(fs);
 
-// const notes = require('./notes');
-// var _= require('lodash');
-// console.log('server file is available');
 
-// var age =notes.age;
-
-// var result = notes.addnumber(age+20,10);
-// console.log(age);
-// console.log('result is '+ result);
-
-// var data =["person","person",1,2,1,2,'name','age','2'];
-// var filter =_.uniq(data);
-// console.log(filter);
-
-// console.log(_.isString(true));
 
 // ---------------55555
 const express =require('express');
@@ -39,8 +15,7 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
-// const Person = require('./models/Person');
-// const MenuItem = require('./models/MenuItem');
+
 
 
 
@@ -61,25 +36,10 @@ app.use('/menu', menuItemRoutes);
 
 
 
-// // app.get('/chiken', (req,res)=>{
-// //     res.send('sure sir, i would love to serve chiken')
-// // })
+
 app.listen(3000 , ()=>{
     console.log('listening on port 3000');
 })
 
-// const express =require('express');
-// const app = express();
 
-// const db =require('./db');
-// const connectDb = require('./db');
-
-// app.use(express.json());
-// const PORT =5000;
-
-// connectDb().then(() =>{
-//     app.listen(PORT, () =>{
-//         console.log('server is port: ${PORT}');
-//     });
-// });
 
